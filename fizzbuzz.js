5fb557d5d3a498248a75ee32cc2f970a6e079ba4
manuelmenzella <manuelmenzella@Manuels-iMac.local>
2014-03-13 22:37:00 -0300

    Commit

---
+++ fizzbuzz.js
@@ -0,0 +1,16 @@
+function fizzbuzz(lowerBound, upperBound) {
+    var str = "";
+    var i, a, b;
+    for (i=lowerBound; i<= upperBound; i++) {
+        a = (i % 3 == 0);
+        b = (i % 5 == 0)
+        if (a)       str += "Fizz";
+        if (b)       str += "Buzz";
+        if (!(a||b)) str += i;
+        str += "\n";
+    }
+    return str;
+}
+
+console.log( fizzbuzz(1, 100) );
+//alert( fizzbuzz(1, 100) );
